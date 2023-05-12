e4a5e5f184fcc5d2f786478d1786b46d170c9087
Семенов Кирилл Александрович <kirsan@nita.ru>
2023-05-12 11:07:06 +0300

    test commit from another place

--- main/main.py
+++ main/main.py
@@ -1,13 +1,11 @@
-from setuptools.config._validate_pyproject.formats import url
+#from setuptools.config._validate_pyproject.formats import url
 
 from src.model import *
 
 if __name__ == '__main__':
     m1 = Model_HH({"text": "python", "area": 2})
     m1.get_data_from_API()
-    #m1.print_content()
-
-
+    # m1.print_content()
 
     m1.write_to_file()
     m1.load_from_file()
