b01d5793991fa3e920600893492167de94a730d4
kirsanwork <kirill_home@mail.ru>
2023-05-18 09:57:33 +0300

    add test_vacancy.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from src.vacancy import *
 
 vac1_for_test = Vacancy(title="title1", desc="title1 desc", salary_from=50_000, salary_to=60_000, url="http://example.com")
 vac2_for_test = Vacancy(title="title2", desc="title2 desc", salary_from=40_000, salary_to=180_000, url="http://example.org")
+vac3_bad_test = Vacancy(title="ti3", desc="title3 desc", salary_from=None, salary_to=None, url="http://example.org")
 
 
 
@@ -14,6 +15,9 @@ vac2_for_test = Vacancy(title="title2", desc="title2 desc", salary_from=40_000, 
 def get_one_test_vacancy():
     return vac1_for_test
 
+@pytest.fixture
+def get_one_bad_vacancy():
+    return vac3_bad_test
 
 @pytest.fixture
 def get_vacancy_test_list():
--- tests/test_vacancy.py
+++ tests/test_vacancy.py
@@ -2,3 +2,26 @@
 
 def test_prints(get_one_test_vacancy):
     assert str(get_one_test_vacancy) == 'title1 with salary from 50000 to 60000'
+    assert str(get_one_test_vacancy) == 'title1 with salary from 50000 to 60000'
+    assert repr(get_one_test_vacancy) == "Vacancy('title1', 50000-60000, http://example.com)"
+
+def test_comparissions(get_vacancy_test_list):
+    assert get_vacancy_test_list[0] >= get_vacancy_test_list[1]
+    assert get_vacancy_test_list[0] > get_vacancy_test_list[1]
+    assert get_vacancy_test_list[1] < get_vacancy_test_list[0]
+    assert get_vacancy_test_list[1] <= get_vacancy_test_list[0]
+    assert (get_vacancy_test_list[0] == get_vacancy_test_list[1]) == False
+    assert (get_vacancy_test_list[0] != get_vacancy_test_list[1]) == True
+
+def test_get_json_format(get_one_test_vacancy):
+    assert get_one_test_vacancy.get_json() == {'description': 'title1 desc',
+                                                 'salary_max': 60000,
+                                                 'salary_min': 50000,
+                                                 'title': 'title1',
+                                                 'url': 'http://example.com'}
+
+def test_fill_empty(get_one_bad_vacancy):
+    get_one_bad_vacancy.check_empty_salary()
+    assert get_one_bad_vacancy.salary_min != None
+    assert get_one_bad_vacancy.salary_max != None
+    assert get_one_bad_vacancy.salary_max == 0
