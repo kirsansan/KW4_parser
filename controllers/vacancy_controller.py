79cfda8eca566599441f94a5f64f7e6f010c8260
kirsansan <kirill_home@mail.ru>
2023-05-19 23:34:54 +0300

    fix little missing docstring =)

--- controllers/vacancy_controller.py
+++ controllers/vacancy_controller.py
@@ -9,12 +9,15 @@ class VacancyController:
         self.view = view
 
     def print_all(self):
+        """Print all information"""
         return self.view.print_all(self.model.all)
 
     def print_ext(self):
+        """Print extended mode"""
         return self.view.print_ext_mode(self.model.all)
 
     def set_and_print_select(self):
+        """set filters for data before printing"""
         self.view.set_filters_dialog()
         prepared_for_print = self.model.filter_by_strings(self.view.filter_keyword.split())
         prepared_for_print = self.model.filter_by_salary(self.view.filter_by_salary, prepared_for_print)
